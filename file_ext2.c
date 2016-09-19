a33ecfe6c541c5c62c30f50d86debc003887a8b9
Venkateshv377 <venkateshv377@gmail.com>
2016-09-19 22:54:02 +0530

    Updated the file to read the directories recursively

--- file_ext2.c
+++ file_ext2.c
@@ -1,6 +1,7 @@
 #include "header.h"
 
-unsigned int block_size = 0;
+unsigned int block_size;
+int local_inode_index;
 unsigned int dir_count, file_count;
 unsigned int group_count, descr_list_size;
 struct ext2_super_block super;
@@ -33,7 +34,7 @@ int main(int argc, char *argv[])
 	}
 
 	block_size = 1024 << super.s_log_block_size;
-	printf("Reading super-block from device: %s\n", argv[1]);
+/*	printf("Reading super-block from device: %s\n", argv[1]);
 	printf("Inodes count            : %x\n", super.s_inodes_count);
 	printf("Blocks count            : %x\n", super.s_blocks_count);
 	printf("Reserved blocks count   : %x\n", super.s_r_blocks_count);
@@ -45,45 +46,41 @@ int main(int argc, char *argv[])
 	printf("Inodes per group        : %x\n", super.s_inodes_per_group);
 	printf("Creator OS              : %x\n", super.s_creator_os);
 	printf("First non-reserved inode: %x\n", super.s_first_ino);
-	printf("Size of inode structure : %x\n", super.s_inode_size);
+	printf("Size of inode structure : %x\n", super.s_inode_size);*/
 	// calculate number of block groups on the disk 
 	group_count = 1 + (super.s_blocks_count-1) / super.s_blocks_per_group;
 
 	// calculate size of the group descriptor list in bytes 
 	descr_list_size = group_count * sizeof(struct ext2_group_desc);
-	printf("sizeof struct ext2_group_desc: %d\n", sizeof(struct ext2_group_desc));
+/*	printf("sizeof struct ext2_group_desc: %d\n", sizeof(struct ext2_group_desc));
 	printf("size of the group descriptor list:	%d\n", descr_list_size);
-	printf("group count:	%x\n", group_count);
+	printf("group count:	%x\n", group_count);*/
 
 	lseek(fd, BASE_OFFSET + block_size, SEEK_SET);
 	read(fd, &group, sizeof(group));
 
-	printf("Inodes table block : %x\n", group.bg_inode_table);
+/*	printf("Inodes table block : %x\n", group.bg_inode_table);
 	printf("Blocks bitmap block: %x\n", group.bg_block_bitmap);
 	printf("Inodes bitmap block: %x\n", group.bg_inode_bitmap);
 	printf("Free blocks count  : %x\n", group.bg_free_blocks_count);
 	printf("Free inodes count  : %x\n", group.bg_free_inodes_count);
-	printf("Directories count  : %x\n", group.bg_used_dirs_count);
+	printf("Directories count  : %x\n", group.bg_used_dirs_count);*/
 	inode_bitmap = group.bg_inode_table * 0x400;
-	printf("inode_bitmap: %x\n", inode_bitmap);
 
-	read_inode(fd, 2, &group, &inode);
+	int inode_no = 2;
+	lseek(fd, BLOCK_OFFSET(group.bg_inode_table)+(inode_no-1)*sizeof(struct ext2_inode), SEEK_SET);
+	read(fd, &inode, sizeof(struct ext2_inode));
+	inode_table(fd, &inode);
+	printf("\nNumber of file:		%d", file_count);
+	printf("\nNumber of directories:	%x\n", dir_count);
 }
 
-void read_inode(int fd, int inode_no, struct ext2_group_desc *group, struct ext2_inode *inode)
-{
-	lseek(fd, BLOCK_OFFSET(group->bg_inode_table)+(inode_no-1)*sizeof(struct ext2_inode), SEEK_SET);
-	printf("offset: %x\n", BLOCK_OFFSET(group->bg_inode_table)+(inode_no-1)*sizeof(struct ext2_inode));
-	read(fd, inode, sizeof(struct ext2_inode));
-	inode_table(fd, inode, group);
-}
-
-void inode_table(int fd, struct ext2_inode *inode, struct ext2_group_desc *group)
+void inode_table(int fd, struct ext2_inode *inode)
 {
 	int inode_bitmap;
 	static struct ext2_inode temp_inode;
 	static int inode_number;
-	struct ext2_dir_entry_2 *entry = NULL;
+	struct ext2_dir_entry_2 *entry;
 	int i = 0;
 	unsigned char *name;
 	unsigned int size = 0;
@@ -92,62 +89,35 @@ void inode_table(int fd, struct ext2_inode *inode, struct ext2_group_desc *group
 		perror("Error:\n");
 
 	lseek(fd, inode->i_block[i] * 0x400, SEEK_SET);
-	printf("inode-block[i]: %x\n",inode->i_block[i] * 0x400);
-	printf("temp_inode: %x\n", inode->i_block[i]);
 	read(fd, name, block_size);
 	entry = (struct ext2_dir_entry_2 *)name;
-	printf("entry->inode: %x\n", entry->inode);
 
-	for(i=0; i<EXT2_N_BLOCKS; i++){
-		if (i < EXT2_NDIR_BLOCKS)         /* direct blocks */
-			printf("Block %2u : %u\n", i, inode->i_block[i]);
-		else if (i == EXT2_IND_BLOCK)     /* single indirect block */
-			printf("Single   : %u\n", inode->i_block[i]);
-		else if (i == EXT2_DIND_BLOCK)    /* double indirect block */
-			printf("Double   : %u\n", inode->i_block[i]);
-		else if (i == EXT2_TIND_BLOCK)    /* triple indirect block */
-			printf("Triple   : %u\n", inode->i_block[i]);
-	}
-	printf("inode->i_size: %x\n", inode->i_size);
-
-	while((size < inode->i_size) && entry->inode) { 
-		//	printf("inode->i_size: %x\tentry->inode: %x\tentry->rec_len: %x\n", inode->i_size, entry->inode, entry->rec_len);
-		if (entry->file_type == 0x02)
+	while((size < inode->i_size) && entry->inode) 
+	{ 
+		if (entry->file_type == 0x02 && (strcmp(entry->name, ".") && strcmp(entry->name, "..")))
 		{
-			if (strcmp(entry->name, ".") && strcmp(entry->name, ".."))
-			{
-				printf("inode_temp:	%x\t", inode_temp);
-				printf("entry->inode:	%x\t", entry->inode);
-				printf("\033[22;36m%s\033[0m\n", entry->name);
-				dir_count++;
+			printf("\033[22;36m%s\033[0m", entry->name);
+			dir_count++;
 
-				if (entry->inode > inode_temp)
-				{
-					group_count = 1 + ((entry->inode-1) / super.s_inodes_per_group);
-					printf("group count:	%d\n", group_count);
-					lseek(fd, BLOCK_OFFSET(group_count) + 32, SEEK_SET);
-					printf("addr: %x\n", BLOCK_OFFSET(group_count) + 32);
-					read(fd, group, sizeof(struct ext2_group_desc));
-					inode_bitmap = group->bg_inode_table * 0x400;
-					printf("inode_bitmap: %x\n", inode_bitmap);
-					lseek(fd, inode_bitmap, SEEK_SET);
-					read(fd, &temp_inode, sizeof(struct ext2_inode));
-					printf("temp_inode: %x\n", temp_inode.i_block[0]);
-					inode_temp = entry->inode;
-					sleep(1);
-					inode_table(fd, &temp_inode, group);
-				}
+			if (entry->inode > inode_temp)
+			{
+				group_count = ((entry->inode) / super.s_inodes_per_group);
+				local_inode_index = (entry->inode) % super.s_inodes_per_group;
+				lseek(fd,1024 + block_size + (group_count * 32) + 8,SEEK_SET);
+				unsigned int addr;
+				read(fd,&addr,4);
+				lseek(fd,(addr * block_size) + ((local_inode_index -1 )* 0x80),SEEK_SET);
+				read(fd, &temp_inode, sizeof(struct ext2_inode));
+				inode_temp = entry->inode;
+				inode_table(fd, &temp_inode);
 			}
 		}
 		else if (entry->file_type == 1)
 		{
-			printf("entry->inode:	%x\t", entry->inode);
-			printf("\033[22;32m%s\033[0m\n", entry->name);
+			printf("\033[22;32m%s\033[0m", entry->name);
 			file_count++;
 		}
 		entry = (void*) entry + entry->rec_len; 
 		size += entry->rec_len;
 	}
-	printf("Number of file:		%d\n", file_count);
-	printf("Number of directories:	%x\n", dir_count);
 }
